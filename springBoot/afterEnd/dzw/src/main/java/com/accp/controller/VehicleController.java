2547d97bdf2990a5cb3d5a4147860e0893d703ac
zjia-0 <2836846329@qq.com>
2020-02-06 22:42:26 +0800

    曾佳，客服——车资料

---
+++ springBoot/afterEnd/dzw/src/main/java/com/accp/controller/VehicleController.java
@@ -0,0 +1,42 @@
+package com.accp.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.accp.domain.Carinfo;
+import com.accp.service.VehicleService;
+
+@RestController
+@RequestMapping("/vehicle")
+public class VehicleController {
+
+	@Autowired
+	VehicleService vehicleService;
+	
+	//查询所有车资料
+	@GetMapping("/queryCarinfo")
+	public List<Carinfo> queryCarinfo(){
+		return vehicleService.queryCarinfo();
+	}
+	//新增车辆信息
+	@GetMapping("/insertCarinfo")
+	public int insertCarinfo(Carinfo carinfo) {
+		return vehicleService.insertCarinfo(carinfo);
+	}
+	//修改车辆信息
+	@GetMapping("/updateCarinfo")
+	public int updateCarinfo(Carinfo carinfo) {
+		return vehicleService.updateCarinfo(carinfo);
+	}
+	//删除车辆信息
+	@GetMapping("/deleteCarinfo")
+	public int deleteCarinfo(String cdlicense) {
+		return vehicleService.deleteCarinfo(cdlicense);
+	}
+	//按驾驶员姓名/简码/手机/车牌号模糊查询
+	//按车牌号与车架号多条件查询
+}
---
+++ springBoot/afterEnd/dzw/src/main/java/com/accp/service/VehicleService.java
@@ -0,0 +1,38 @@
+package com.accp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.accp.domain.Carinfo;
+import com.accp.mapper.CarinfoMapper;
+
+@Service
+@Transactional
+public class VehicleService {
+	
+	@Autowired
+	CarinfoMapper carinfoMapper;
+	
+	//查询所有车资料
+	public List<Carinfo> queryCarinfo(){
+		return carinfoMapper.selectByExample(null);
+	}
+	//新增车辆信息
+	public int insertCarinfo(Carinfo carinfo) {
+		return carinfoMapper.insert(carinfo);
+	}
+	//修改车辆信息
+	public int updateCarinfo(Carinfo carinfo) {
+		return carinfoMapper.updateByPrimaryKey(carinfo);
+	}
+	//删除车辆信息
+	public int deleteCarinfo(String cdlicense) {
+		return carinfoMapper.deleteByPrimaryKey(cdlicense);
+	}
+	//按驾驶员姓名/简码/手机/车牌号模糊查询
+	//按车牌号与车架号多条件查询
+}
