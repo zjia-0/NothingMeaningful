00d52c3d66bf3a104a2865068c57fe869227d267
JZhengMing <214293252@qq.com>
2020-02-15 17:56:10 +0800

    蒋铮铭--班组技工（第二次提交）

--- springBoot/afterEnd/dzw/src/main/java/com/accp/controller/BzjgController.java
+++ springBoot/afterEnd/dzw/src/main/java/com/accp/controller/BzjgController.java
@@ -8,14 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accp.domain.Banzu;
+import com.accp.domain.Personnel;
 import com.accp.domain.bzjg;
 import com.accp.service.BzjgService;
+import com.accp.service.ZzjgService;
 
 @RestController
 public class BzjgController {
 
 	@Autowired
 	BzjgService bser;
+	
+	@Autowired
+	ZzjgService zser;
 	
 	//查询班组树状图
 	@RequestMapping("/bzszt")
@@ -36,4 +41,30 @@ public class BzjgController {
 	}
 	
 
+	//新增员工
+	@RequestMapping("/addjg")
+	public int addjg(Personnel per,String workid,String stars,String craft,String brand,String remark1,String remark2) {
+		return bser.addjg(per, workid, stars, craft, brand, remark1, remark2);
+	}
+	
+	//新增模态框查询班组id
+	@RequestMapping("/addbanzuid")
+	public Banzu addbanzuid(String id) {
+			return bser.addbanzuid(id);
+	}
+	
+	//根据id查询技工修改
+	@RequestMapping("/jgxiugaiid")
+	public bzjg jgxiugai(String workid) {
+		return bser.jgxiugai(workid);
+	}
+	
+
+
+	//修改员工
+	@RequestMapping("/updatejg")
+	public int updatejg(Personnel per,String workid,String stars,String craft,String brand,String remark1,String remark2) {
+		return bser.updatejg(per, workid, stars, craft, brand, remark1, remark2);
+	}
+	
 }
